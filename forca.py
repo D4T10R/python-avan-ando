489e222453e4adaf9b13f6015ace4ee2fa4cb936
D4T10R <leonardowederveiga@yahoo.com>
2023-08-13 10:30:39 -0300

    Implementando o encerramento do jogo

--- forca.py
+++ forca.py
@@ -3,29 +3,46 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = 'maça'.upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(f"{letras_acertadas}")
 
     while (not enforcou and not acertou):
         print('jogando...')
         chute = input('Qual letra você deseja chutar? ')
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                print(f"Encontrei a letra {letra} na posição {index}")
-                letras_acertadas[index] = letra
-            index = index + 1
-        print(letras_acertadas)
 
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    print(f"Encontrei a letra {letra} na posição {index}")
+                    letras_acertadas[index] = letra
+                index += 1
+        else: 
+            erros += 1
+        
+        print(letras_acertadas)
         if (palavra_secreta.find(chute) == -1):
             print(f"a leta {chute} não existe na plavra")
+
+        if (erros == 6):
+            print("VOCÊ SE ENFORCOU")
+            enforcou = True
+        elif ("_" not in letras_acertadas):
+            print("VOCÊ GANHOU !!!")
+            acertou = True
+
+       
+
+       
+       
 
     print("Fim do jogo")
 
